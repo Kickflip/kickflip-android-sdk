a537b397e171abec4d16e8eca0a0f7b8bf3e9b56
David Brodsky <dbro@dbro.pro>
2014-04-03 14:39:23 -0700

    rm comment

--- sdk/src/main/java/io/kickflip/sdk/api/json/HlsStream.java
+++ sdk/src/main/java/io/kickflip/sdk/api/json/HlsStream.java
@@ -43,16 +43,4 @@ public class HlsStream extends Stream {
         return "Bucket: " + getBucket() + " streamUrl " + getStreamUrl();
     }
 
-//    {
-//        "aws_secret_key": "0TJw99/WtbG3ovY6CzsiYXl8/H2+0BrAxHPVoVge",
-//            "aws_access_key": "AKIAIHAIKHSD53WPYKWQ",
-//            "success": true,
-//            "chat_url": "Not implemented yet",
-//            "stream_type": "HLS",
-//            "kickflip_url": "https://app.kickflip.io/542c4554-d7b8-4ea6-bdd7-251a8f4df764",
-//            "bucket_name": "android-test-c3fec7e0-d40f-495c-a25b-1fae44cd5e5d",
-//            "streamID": "542c4554-d7b8-4ea6-bdd7-251a8f4df764",
-//            "stream_url": "https://android-test-c3fec7e0-d40f-495c-a25b-1fae44cd5e5d.s3.amazon.com/android-test-c3fec7e0-d40f-495c-a25b-1fae44cd5e5d-n20rj1/542c4554-d7b8-4ea6-bdd7-251a8f4df764/index.m3u8",
-//            "upload_url": "https://android-test-c3fec7e0-d40f-495c-a25b-1fae44cd5e5d.s3.amazon.com/android-test-c3fec7e0-d40f-495c-a25b-1fae44cd5e5d-n20rj1/542c4554-d7b8-4ea6-bdd7-251a8f4df764/"
-//    }
 }
