b2d4694b584733b92bfdd467d1de8347703fef0c
uberspot <onexemailx@gmail.com>
2014-05-09 20:41:06 +0300

    fix immersiveMode

--- sdk/src/main/java/io/kickflip/sdk/activity/ImmersiveActivity.java
+++ sdk/src/main/java/io/kickflip/sdk/activity/ImmersiveActivity.java
@@ -15,6 +15,12 @@ public abstract class ImmersiveActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         hideSystemUi();
     }
 
@@ -29,10 +35,11 @@ public abstract class ImmersiveActivity extends Activity {
     }
 
     private void hideSystemUi() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         if (!isKitKat() || !mUseImmersiveMode) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else if (mUseImmersiveMode) {
+            setKitKatWindowFlags();
         }
     }
 
@@ -41,15 +48,20 @@ public abstract class ImmersiveActivity extends Activity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (isKitKat() && hasFocus && mUseImmersiveMode) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            setKitKatWindowFlags();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void setKitKatWindowFlags() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            );
-        }
+        );
     }
 
     private boolean isKitKat() {
