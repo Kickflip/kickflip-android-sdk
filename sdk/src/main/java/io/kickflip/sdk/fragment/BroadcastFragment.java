9048ae6f10f417ba17429adbc551ba2378f5d9b3
David Brodsky <dbro@dbro.pro>
2014-05-27 11:26:14 -0700

    BroadcastFragment getActivity() != null check

--- sdk/src/main/java/io/kickflip/sdk/fragment/BroadcastFragment.java
+++ sdk/src/main/java/io/kickflip/sdk/fragment/BroadcastFragment.java
@@ -339,13 +339,15 @@ public class BroadcastFragment extends Fragment implements AdapterView.OnItemSel
     }
 
     private void setBannerToLiveState(String watchUrl) {
-        mLiveBanner.setBackgroundResource(R.drawable.live_red_bg);
-        Drawable img = getActivity().getResources().getDrawable(R.drawable.ic_share_white);
-        mLiveBanner.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
-        if (watchUrl != null) {
-            mLiveBanner.setTag(watchUrl);
+        if (getActivity() != null) {
+            mLiveBanner.setBackgroundResource(R.drawable.live_red_bg);
+            Drawable img = getActivity().getResources().getDrawable(R.drawable.ic_share_white);
+            mLiveBanner.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+            if (watchUrl != null) {
+                mLiveBanner.setTag(watchUrl);
+            }
+            mLiveBanner.setText(getString(R.string.live));
         }
-        mLiveBanner.setText(getString(R.string.live));
     }
 
     private void animateLiveBanner() {
